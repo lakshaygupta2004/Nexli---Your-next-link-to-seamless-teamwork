cd197ae9953719ec21dd8418a5cb368c770c03f0
Lakshay Gupta <165468654+lakshaygupta2004@users.noreply.github.com>
2025-08-29 20:41:33 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,45 @@
+import "../instrument.mjs";
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
+import chatRoutes from "./routes/chat.route.js";
+import cors from "cors";
+import * as Sentry from "@sentry/node";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // req.auth will be available in the request object
+
+app.get("/debug-sentry", (req, res) => {
+  throw new Error("My first Sentry error!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Nexli API 🚀");
+});
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+  console.log(`🚀 Nexli server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+    });
+  } catch (error) {
+    console.error("❌ Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+
+export default app;
